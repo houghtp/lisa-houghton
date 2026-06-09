2f2be76001490aad5461f0229fbc379412aa8d99
Paul Houghton <houghtonpj@googlemail.com>
2026-06-09 18:55:49 +0100

    feat: register portfolioWork and jobListing schemas

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -1,4 +1,6 @@
 import { journalPost } from './journalPost'
 import { fashionEvent } from './fashionEvent'
+import { portfolioWork } from './portfolioWork'
+import { jobListing } from './jobListing'
 
-export const schemaTypes = [journalPost, fashionEvent]
+export const schemaTypes = [journalPost, fashionEvent, portfolioWork, jobListing]

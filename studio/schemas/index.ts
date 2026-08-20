54f51c9c2071e6d9e8031b417762dbd946a40b61
Paul Houghton <houghtonpj@googlemail.com>
2026-08-20 12:14:49 +0100

    Update index.ts

--- studio/schemas/index.ts
+++ studio/schemas/index.ts
@@ -2,5 +2,6 @@ import { journalPost } from './journalPost'
 import { fashionEvent } from './fashionEvent'
 import { portfolioWork } from './portfolioWork'
 import { jobListing } from './jobListing'
+import { newsletterIssue } from './newsletterIssue'
 
-export const schemaTypes = [journalPost, fashionEvent, portfolioWork, jobListing]
+export const schemaTypes = [journalPost, fashionEvent, portfolioWork, jobListing, newsletterIssue]
